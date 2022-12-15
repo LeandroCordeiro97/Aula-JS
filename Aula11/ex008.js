4b2f6f2bcceaefb3a38c495b70d4a8dff15d8ecc
Leandro Martins Cordeiro <leandrocordeiro97@live.com.pt>
2022-12-15 19:43:53 +0000

    atualizacao

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 65
+    console.log (`A velocidade do carro é ${vel} km/h`)
+if(vel > 60){  //Condição Simples
+    console.log (`Voce ultrapassou a velocidade permitida`)
+}
+    console.log (`Dirija sempre usando cinto de segurança`)
---
+++ ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Portugall'
+if (pais == 'Brasil') {
+    console.log ('voce é brasileiro')
+}
+else {
+    console.log ('voce é estrangeiro')
+}
